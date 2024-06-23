2c8d625d5ecbf77e403fce79d3447285f27467c4
luizeduardoraposo <luizeduardoraposo@protonmail.com>
2024-06-23 20:22:44 -0300

    fix namespaces of packages

--- src/alura/java/desafios/Perfil.java
+++ src/alura/java/desafios/Perfil.java
@@ -1,6 +1,10 @@
-public void main() {
+package alura.java.desafios;
+
+public class Perfil{
+    public void main() {
     System.out.println("Olá Luiz Eduardo Raposo");
     System.out.println("Tudo bem?");
     System.out.println(10 + 5);
     System.out.println(10 - 5);
+}
 }
---
+++ src/alura/java/desafios/screenMatch.java
@@ -0,0 +1,11 @@
+package alura.java.desafios;
+
+public class screenMatch {
+    public void main() {
+        System.out.println("Esse é o Screen Match");
+        System.out.println("Filme: Top Gun: Maverick");
+        int releaseYear = 2022;
+        System.out.println(STR."Release Year: \{releaseYear}");
+        boolean includeInPLan = true;
+    }
+}
