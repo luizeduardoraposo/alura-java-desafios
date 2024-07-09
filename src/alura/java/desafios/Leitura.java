65caf07a543804a9f010aca1ea01c6cfe1195662
luizeduardoraposo <luizeduardoraposo@protonmail.com>
2024-07-09 00:03:02 -0300

    add class Scanner

--- src/alura/java/desafios/Leitura.java
+++ src/alura/java/desafios/Leitura.java
@@ -7,5 +7,8 @@ public class Leitura {
         System.out.println("Quais seus livros favoritos?");
         String filme = Leitura.nextLine();
         System.out.println(filme);
+        System.out.println("Quais são os número naturais?");
+        int Naturais = Leitura.nextInt();
+        System.out.println(Naturais);
     }
 }
