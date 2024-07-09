90256c8c3a35d4fa052c600a4af69b516899f11f
luizeduardoraposo <luizeduardoraposo@protonmail.com>
2024-07-09 01:37:37 -0300

    loop for and while

---
+++ src/alura/java/desafios/Loop.java
@@ -0,0 +1,17 @@
+package alura.java.desafios;
+import java.util.Scanner;
+
+public class Loop {
+    public void main(){
+    Scanner leitura = new Scanner(System.in);
+    double mediaAvaliacao = 0;
+    double nota;
+
+    for (int i = 0; i < 3; i++) {
+        System.out.println("Avaliação do filme?");
+        nota = leitura.nextDouble();
+        mediaAvaliacao += nota;
+    }
+    System.out.println("Media de avaliacoes para o filme é : " +mediaAvaliacao/3);
+    }
+}
