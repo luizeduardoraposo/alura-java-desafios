250beb2a89b2bce6bc24e257c073822132b07cb9
luizeduardoraposo <luizeduardoraposo@protonmail.com>
2024-07-09 13:57:52 -0300

    Opcões 1 e 2

---
+++ src/alura/java/desafios/DesafioAlura003.java
@@ -0,0 +1,39 @@
+package alura.java.desafios;
+
+import java.util.Scanner;
+
+public class DesafioAlura003 {
+    public static void main(){
+        String nome = "Cliente Teste";
+        String tipoConta = "Conta Corrente";
+        double saldo = 7500;
+        int opcao = 0;
+        System.out.println("****************");
+        System.out.println("Nome do Cliente: " + nome);
+        System.out.println("Tipo de Conta: " + tipoConta);
+        System.out.println("Saldo: " + saldo);
+        System.out.println("*****************");
+
+        String menu = """
+                Digite sua opção:
+                1 - Consultar Saldo
+                2 - Transferir Valor
+                3 - Receber Valor
+                4 - Sair
+                """;
+        Scanner leitura = new Scanner(System.in);
+        while(opcao != 4){
+            System.out.println(menu);
+            opcao = leitura.nextInt();
+
+            if (opcao == 1){
+                System.out.println("O saldo atualizado é " + saldo);
+            } else if(opcao == 2){
+                System.out.println("Qual o valor deseja transferir?");
+                double valor = leitura.nextDouble();
+            }
+
+        }
+
+    }
+}
