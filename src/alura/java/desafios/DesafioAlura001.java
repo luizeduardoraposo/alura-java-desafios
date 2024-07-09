8098c13e28ae40eff03649549cc8f689065aae24
luizeduardoraposo <luizeduardoraposo@protonmail.com>
2024-07-09 02:37:04 -0300

    erro de Alice

---
+++ src/alura/java/desafios/DesafioAlura001.java
@@ -0,0 +1,14 @@
+package alura.java.desafios;
+
+public class DesafioAlura001 {
+
+    public static void main(String[] args) {
+        int contador = 1;
+
+        while(contador <= 10) {
+            System.out.println(contador);
+            contador++;
+
+        }
+    }
+}
