61f6ac7fb8b2651286f96c28e79f29ba0385e30d
luizeduardoraposo <luizeduardoraposo@protonmail.com>
2024-07-08 23:55:19 -0300

    add class Scanner

---
+++ src/alura/java/desafios/Leitura.java
@@ -0,0 +1,11 @@
+package alura.java.desafios;
+import java.util.Scanner;
+
+public class Leitura {
+    public void main(){
+        Scanner Leitura = new Scanner(System.in);
+        System.out.println("Quais seus livros favoritos?");
+        String filme = Leitura.nextLine();
+        System.out.println(filme);
+    }
+}
