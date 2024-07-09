1d89a5596e9e5e1fa2d2369c06dc59b0b1d92535
luizeduardoraposo <luizeduardoraposo@protonmail.com>
2024-07-09 02:37:43 -0300

    refatorar while para for

---
+++ src/alura/java/desafios/DesafioAlura002.java
@@ -0,0 +1,14 @@
+package alura.java.desafios;
+
+public class DesafioAlura002 {
+
+    public static void main(String[] args) {
+
+        int contador = 1;
+
+        for(int i = 1; i <= 10; i++) {
+            System.out.println(contador);
+            contador++;
+        }
+    }
+}
