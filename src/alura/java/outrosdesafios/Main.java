3639ceadd422a2c9eb7fb9dd5426ddc28e528c7a
luizeduardoraposo <luizeduardoraposo@protonmail.com>
2024-07-09 22:10:27 -0300

    update saldo

--- src/alura/java/outrosdesafios/Main.java
+++ src/alura/java/outrosdesafios/Main.java
@@ -3,7 +3,17 @@ package alura.java.outrosdesafios;
 public class Main {
     public static void main(String[] args) {
         Conta conta = new Conta();
+
         conta.deposita(275);
+        System.out.println(conta.getSaldo());
+
+        conta.setAgencia(666);
+        System.out.println(conta.getAgencia());
+
+        conta.setNumero(777);
+        System.out.println(conta.getNumero());
+
+        conta.setSaldo(123);
         System.out.println(conta.getSaldo());
     }
 }
